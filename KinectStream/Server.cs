591aa5a1dc62b63cc2b1e3142724efd351a458c2
Progman-nkk <pneucolas@gmail.com>
2018-02-19 18:38:39 -0500

    backing up

---
+++ KinectStream/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothStream
+{
+
+    class Server
+    {
+        System.Net.IPEndPoint endPoint;
+        TcpListener listener;
+        public Server(int portNumber, long address)
+        {
+            endPoint = new System.Net.IPEndPoint(address, portNumber);
+            listener = new TcpListener(endPoint);
+            listener.Start();
+        }
+
+    }
+}
